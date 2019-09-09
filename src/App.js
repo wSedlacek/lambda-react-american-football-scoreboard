552e1dad36bf3028f191efc6b721e7185d01587b
William Sedlacek <wsedlacekc@gmail.com>
2019-09-09 13:19:12 -0500

    Add score handler

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import './App.css';
 import BottomRow from './BottomRow';
 
 function App() {
-  const [homeScore, setHomeScore] = useState(0);
-  const [awayScore, setAwayScore] = useState(0);
+  const homeHook = useState(0);
+  const awayHook = useState(0);
+
+  const scoreHandler = (hook, add) => {
+    hook[1](hook[0] + add);
+  };
 
   return (
     <div className='container'>
@@ -13,30 +17,30 @@ function App() {
           <div className='home'>
             <h2 className='home__name'>Lions</h2>
 
-            <div className='home__score'>{homeScore}</div>
+            <div className='home__score'>{homeHook[0]}</div>
           </div>
           <div className='timer'>00:03</div>
           <div className='away'>
             <h2 className='away__name'>Tigers</h2>
-            <div className='away__score'>{awayScore}</div>
+            <div className='away__score'>{awayHook[0]}</div>
           </div>
         </div>
         <BottomRow />
       </section>
       <section className='buttons'>
         <div className='homeButtons'>
-          <button className='homeButtons__touchdown' onClick={() => setHomeScore(homeScore + 7)}>
+          <button className='homeButtons__touchdown' onClick={() => scoreHandler(homeHook, 7)}>
             Home Touchdown
           </button>
-          <button className='homeButtons__fieldGoal' onClick={() => setHomeScore(homeScore + 3)}>
+          <button className='homeButtons__fieldGoal' onClick={() => scoreHandler(homeHook, 3)}>
             Home Field Goal
           </button>
         </div>
         <div className='awayButtons'>
-          <button className='awayButtons__touchdown' onClick={() => setAwayScore(awayScore + 7)}>
+          <button className='awayButtons__touchdown' onClick={() => scoreHandler(awayHook, 7)}>
             Away Touchdown
           </button>
-          <button className='awayButtons__fieldGoal' onClick={() => setAwayScore(awayScore + 3)}>
+          <button className='awayButtons__fieldGoal' onClick={() => scoreHandler(awayHook, 3)}>
             Away Field Goal
           </button>
         </div>
